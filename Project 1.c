b3450f25900700a0babf90f3872c188d5df9e14b
Tim <tim.pundt@trojans.dsu.edu>
2024-02-05 13:00:21 -0600

    Complete:
    Added countSort
    
    Added clock

--- Project 1.c
+++ Project 1.c
@@ -7,12 +7,10 @@ void countSort(int A[], int n);
 void printArray(int A[], int n);
 void generateInFile(int n);
 
-const int AMOUNT_OF_GENERATED_NUMBERS = 15;
+const long AMOUNT_OF_GENERATED_NUMBERS = 10;
 
 int main()
 {
-
-    printf("Hello World");
     generateInFile(AMOUNT_OF_GENERATED_NUMBERS);
 
         //Block of code for reading in the file and reading into a dynamic array
@@ -63,7 +61,26 @@ int main()
         //End of A2
         //End of block of code
 
-    selectionSort(A1, NumberOfIntegers);
+    //Define variables, setup clock, and call selectionSort.
+        // clock_t start1, end1;
+        // double time_spent1;
+
+        // start1 = clock();
+        // selectionSort(A1, NumberOfIntegers);
+        // end1 = clock();
+        // time_spent1 = (double)(end1 - start1) / CLOCKS_PER_SEC;
+        // printf("Time taken by selectionSort algorithm is %f sec.\n",time_spent1);
+
+
+    //Define variables, setup clock and call CountSort
+    clock_t start2, end2;
+    double time_spent2;
+
+    start2 = clock();
+    countSort(A2, NumberOfIntegers);
+    end2 = clock();
+    time_spent2 = (double)(end2 - start2) / CLOCKS_PER_SEC;
+    printf("Time taken by countSort algorithm is %f sec.\n", time_spent2);
 
     /*
     1. Call generateInFile function to generate integer random numbers and store them in a text file
@@ -103,6 +120,8 @@ void selectionSort(int A[], int n) // n is the size of A
         }
     }
 
+    printf("Smallest found");
+
     //Swap the min and first index in array
     A[temp_index] = A[0];
     A[0] = min;
@@ -125,21 +144,65 @@ void selectionSort(int A[], int n) // n is the size of A
         A[temp_index] = A[i];
         A[i] = min;
         }
-    }
 
-    printArray(A, n);
+        printf("check %d\n", i);
+    }
 }
 
 //counting sort function
 void countSort(int A[], int n) // n is the size of A
 {
     int k, *B, *C;
-    //Write a code to find the value of k that is equal to the maximum value of A
 
-    B = (int*)malloc(n * sizeof(int)); //Dynamic allocation of two temporarily arrays B and C.
+    //Code to find the value of k that is equal to the maximum value of A
+    k = A[0];
+    for(int i=1; i<n; i++)
+    {
+        if(A[i] > k)
+        {
+            k = A[i];
+        }
+    }
+
+
+
+    //Allocate B and C
+    B = (int*)malloc(n * sizeof(int)); 
     C = (int*)malloc((k+1) * sizeof(int));
 
-    //continue the implementation of the counting sort algorithm here.
+
+    //Initialize all C to 0
+    for(int i=0; i<=k; ++i)
+    {
+        C[i] = 0;
+    }
+
+    //Count the number of times an element occurs and store in C
+    for(int i=0; i<n; ++i)
+    {
+        C[A[i]] = C[A[i]] + 1;
+    }
+
+    // Change C to hold the number of elements less than or equal to i
+    for(int i=1; i<=k; ++i)
+    {
+        C[i] = C[i] + C[i-1];
+    }
+
+    //Arrange the output array
+    for(int i=0; i<n; ++i)
+    {
+        B[C[A[i]] -1] = A[i];
+        C[A[i]] = C[A[i]] - 1;
+    }
+
+    //A = B
+    for(int i=0; i<n; ++i)
+    {
+        A[i] = B[i];
+    }
+
+    //printArray(A, n);
 }
 
 //Function to print the content of an array
@@ -170,7 +233,7 @@ void printArray(int A[], int n)
     srand(time(NULL));
     for (int i = 0; i < n; i++)
     {
-        x = rand() % 100 + 1; //remove the logical statemenet when done testing
+        x = rand(); //                         % 100 + 1; //remove the logical statemenet when done testing
         fprintf(out, "%d ", x);
     }
     fclose(out);
